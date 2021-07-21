b28ecbd2f91bb1f731c0d57e21334dc4c7b25dd5
eichfernandes <rrafael.fernandes@gmail.com>
2021-07-21 15:29:56 -0300

    ADM adicionado e Mudança na Pesquisa e Ordem

---
+++ admin.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table{border-collapse: collapse; width: 100%;}
+            th,td{padding: 5px; text-align: left; border: 1px solid #000;}
+            div{margin: auto; width: 70%; font-family: sans-serif;}
+        </style>
+    </head>
+    <body>
+        <div><h2>Adicionar</h2>
+            <form method="post">
+                <input name="adtitulo" type="text" placeholder="Titulo" size="25">
+                <input name="adano" type="number" placeholder="Ano" min="1900" max="2100">
+                <input name="addiretor" type="text" placeholder="Diretor" size="25">
+                <input name="adnota" type="number" placeholder="Nota" step="0.5" min="0" max="10">
+                <input type="submit" value="Confirmar" />
+            </form>
+            <?php include "conexao.php";
+                if (!empty($_POST["adtitulo"])&&!empty($_POST["adano"])&&!empty($_POST["addiretor"])&&!empty($_POST["adnota"])){
+                    $mysqli->query("insert into movies (titulo,ano,diretor,nota) values ("
+                            . "'" . $_POST["adtitulo"] . "'," . $_POST["adano"] . ","
+                            . "'" . $_POST["addiretor"] . "'," . $_POST["adnota"] . ")");
+                }
+            ?>
+        </div>
+        <div><h2>Remover</h2>
+            <form method="post">
+                <input name="removeid" type="number" placeholder="ID" min="0" max="5000">
+            </form>
+            <br>O Filme com o ID específicado será removido permanentemente.
+            <?php include "conexao.php";
+                if (!empty($_POST["removeid"])){
+                    $mysqli->query("DELETE FROM movies WHERE id=".$_POST["removeid"].";");
+                }
+            ?>
+            <br>
+        </div>
+        <br><br>
+        <!-- Ordenar Conteúdo -->
+        <div>
+            <form method="post" name="proc" style="float: left;">
+                Pesquisar: <input type="text" name="procurar" value="<?php
+                    if (!empty($_POST["procurar"])){echo $_POST["procurar"];};
+                ?>">
+                Ordenar: <select name="ordem" onchange="this.form.submit()">
+                    <option <?php if (empty($_POST["ordem"])||$_POST["ordem"]=="id"){echo "selected";} ?> 
+                        value="id">ID ↓</option>
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="id desc"){echo "selected";} ?> 
+                        value="id desc">ID ↑</option>
+                    
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="titulo"){echo "selected";} ?> 
+                        value="titulo">Título ↓</option>
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="titulo desc"){echo "selected";} ?> 
+                        value="titulo desc">Título ↑</option>
+                    
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="ano"){echo "selected";} ?>
+                        value="ano">Ano ↓</option>
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="ano desc"){echo "selected";} ?>
+                        value="ano desc">Ano ↑</option>
+                    
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="diretor"){echo "selected";} ?>
+                        value="diretor">Diretor ↓</option>
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="diretor desc"){echo "selected";} ?>
+                        value="diretor desc">Diretor ↑</option>
+                    
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="nota"){echo "selected";} ?>
+                        value="nota">Nota ↓</option>
+                    <option <?php if (!empty($_POST["ordem"])&&$_POST["ordem"]=="nota desc"){echo "selected";} ?>
+                        value="nota desc">Nota ↑</option>
+                </select>
+            </form>
+            <script>
+                function submitforms(){
+                    document.forms["list"].submit();
+                }
+            </script>
+            <button style="float: right;" type="button" value="Enviar Mudanças" onclick="submitforms()">Enviar Mudanças</button>
+            <br><br>
+            <form method="post" name="list">  
+            <table>
+                <tr><th>ID</th><th>Título</th><th>Ano</th><th>Diretor</th><th>Nota</th></tr>
+                <?php include "conexao.php";
+                    if (!empty($_POST["procurar"])){$procurar =
+                        " where id like '%".$_POST["procurar"]."%' or titulo like '%".$_POST["procurar"].
+                        "%' or ano like '%".$_POST["procurar"]."%' or diretor like '%".$_POST["procurar"]."%'"
+                    ;}else{$procurar = "";};
+                    if (!empty($_POST["ordem"])){$ordem = " order by ".$_POST["ordem"];}
+                    else {$ordem = " order by id";};
+                    $sql = "SELECT * FROM movies" . $procurar . $ordem;
+                    if ($result = $mysqli->query($sql)) {
+                        /* fetch associative array */
+
+                        while ($row = $result->fetch_assoc()) {
+                            if (!empty($_POST["nota".$row["id"]])){$mysqli->query("UPDATE movies SET nota=".$_POST["nota".$row["id"]]." WHERE id=".$row["id"]);
+                                $row["nota"]=$_POST["nota".$row["id"]];
+                            };
+                            echo utf8_encode(
+                                "<tr><td>" . $row["id"] . "</td><td>" . $row["titulo"] . "</td>"
+                                    . "<td>" . $row["ano"] . "</td>"
+                                    . "<td>" . $row["diretor"] . "</td>"
+                                    . "<td><input type='number' step='0.5' min='0' max='10' placeholder='" . $row["nota"]."' name='nota".$row["id"]."'>"
+                            . "</td></tr>");
+                            
+                        }
+                        $result->close(); /* free result set */
+                    }
+                    $mysqli->close(); /* close connection */
+                ?>
+            </table>
+            </form>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,21 +10,28 @@
         <?php include "header.php" ?>
 	<!--Barra de Navegação-->
         <div id="movies">
-            <?php if (empty($_POST["order"])){$order = "SELECT * FROM movies order by titulo, ano";}
-                    else{$order = $_POST["order"];}; ?>
             <form method="post">
                 <div style="text-align: left; width: 50%; float: left;">
-                    Procurar: <input type="text" name="search" style="width: 50%;" value="<?php 
+                    <input placeholder="Procurar" type="text" name="search" style="width: 50%;" value="<?php 
                         if (!empty($_POST["search"])){echo $_POST["search"];}
                     ?>">
                 </div>
                 <div style="text-align: right; width: 50%; float: right;">Ordenar:
                 <select name="order" id="order" onchange="this.form.submit()">
-                    <option <?php if (empty($_POST["order"])||$_POST["order"]=="SELECT * FROM movies order by titulo, ano"){echo "selected";} ?> value="SELECT * FROM movies order by titulo, ano">Título</option>
-                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="SELECT * FROM movies order by ano, titulo"){echo "selected";} ?> value="SELECT * FROM movies order by ano, titulo">Ano</option>
-                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="SELECT * FROM movies order by diretor, titulo, ano"){echo "selected";} ?> value="SELECT * FROM movies order by diretor, titulo, ano">Diretor</option>
-                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="SELECT * FROM movies order by nota desc, titulo"){echo "selected";} ?> value="SELECT * FROM movies order by nota desc, titulo">Nota</option>
-                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="SELECT * FROM movies order by id desc"){echo "Recentes";} ?> value="SELECT * FROM movies order by id desc">Recentes</option>
+                    <option <?php if (empty($_POST["order"])||$_POST["order"]=="order by titulo"){echo "selected";} ?> value="order by titulo">Título ↓</option>
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by titulo desc"){echo "selected";} ?> value="order by titulo desc">Título ↑</option>
+                    
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by ano"){echo "selected";} ?> value="order by ano">Ano ↓</option>
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by ano desc"){echo "selected";} ?> value="order by ano desc">Ano ↑</option>
+                    
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by diretor"){echo "selected";} ?> value="order by diretor">Diretor ↓</option>
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by diretor desc"){echo "selected";} ?> value="order by diretor desc">Diretor ↑</option>
+                    
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by nota"){echo "selected";} ?> value="order by nota">Nota ↓</option>
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by nota desc"){echo "selected";} ?> value="order by nota desc">Nota ↑</option>
+                    
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by id desc"){echo "selected";} ?> value="order by id desc">Recentes ↓</option>
+                    <option <?php if (!empty($_POST["order"])&&$_POST["order"]=="order by id"){echo "selected";} ?> value="order by id">Recentes ↑</option>
                 </select>
                 </div>
             </form>
@@ -34,24 +41,33 @@
                 const ano = [];
                 function trclick(x,y){
                     window.open("https://www.google.com/search?q="+x+" "+y, "_blank");
-                }
+                };
             </script>
             <br>
             <table>
                 <tr><th>Título</th><th>Ano</th><th>Diretor</th><th>Nota</th></tr>
                 <?php include "conexao.php";
+                    /* Barra de Procura */
+                    if (!empty($_POST["search"])){$search = " where titulo like '%".$_POST["search"]
+                        ."%' or ano like '%".$_POST["search"]."%' or diretor like '%".$_POST["search"]."%'";
+                    }else{
+                        $search="";
+                    }
+                    /* Ordenar */
+                    if (!empty($_POST["order"])){
+                        $order = " ".$_POST["order"];
+                    }else{
+                        $order = " order by titulo";
+                    }
                     
-                    if (!empty($_POST["search"])){$sql="SELECT * FROM movies where titulo like '%".$_POST["search"].
-                            "%' or ano like '%".$_POST["search"]."%' or diretor like '%".$_POST["search"].
-                            "%' order by titulo, ano";}
-                    else {$sql = $order;};
+                    $sql = "SELECT * FROM movies".$search.$order;
                     header('Content-Type: text/html; charset=utf-8');
                     if ($result = $mysqli->query($sql)) {
                         /* fetch associative array */
 
                         while ($row = $result->fetch_assoc()) {
                             echo utf8_encode(
-                                "<script>tit[".$row["id"]."]='".$row["titulo"]." '; ano[".$row["id"]."]='".$row["ano"]."';</script>"
+                                "<script>tit[".$row["id"]."]='".$row["titulo"]."'; ano[".$row["id"]."]='".$row["ano"]."';</script>"
                                     
                                 . "<tr onclick='trclick(tit[".$row["id"]."],ano[".$row["id"]."]);'>"
                                 . "<td>" . $row["titulo"] . "</td><td>" . $row["ano"] . "</td><td>"
